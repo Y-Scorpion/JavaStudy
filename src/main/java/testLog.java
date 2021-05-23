ec837a824ec842fbcc4c9d2aab0c3576dd69705f
宗亚 张 <okzhangzongya@163.com>
2021-05-23 22:58:34 +0800

    Initial commit

---
+++ src/main/java/testLog.java
@@ -0,0 +1,17 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class testLog {
+
+    public static void main(String[] args)
+    {
+        Logger logger = LogManager.getLogger(testLog.class);
+        logger.trace("trace level");
+        logger.debug("debug level");
+        logger.info("info level");
+        logger.warn("warn level");
+        logger.error("error level");
+        logger.fatal("fatal level");
+    }
+}
